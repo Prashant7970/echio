b4f0bdc1a7f3afc609790640df1a859a416827f4
Prashant7970 <prashant887359@gmail.com>
2023-06-02 10:00:34 +0530

    first commit

---
+++ src/app/db.js
@@ -0,0 +1,121 @@
+export const data=[
+   
+    
+    {
+        image: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/1/7/17_14_4.jpg",
+        title: "Vaadi Herbals 24 Carat Gold Massage Gel With 24 Carat",
+        date: "2002",
+        id: "BBJ003250",
+        strikedOffPrice: "₹289",
+        price: 289,
+        discount: "12 % OFF",
+        rate: 1,
+        brand: "Vaadi Herbals",
+
+       
+    },
+    {
+        image: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/b/e/be43.jpg",
+        title: "Beeone Cucumber Facial Gel",
+        date: "2004",
+        id: "BBJ003250",
+        strikedOffPrice: "₹219",
+        price: 219,
+        discount: "12 % OFF",
+        rate: 2,
+        brand: "Vaadi Herbals",
+
+       
+    },
+    {
+        image: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/b/e/be46.jpg",
+        title: "Beeone Diamond Facial Gel 440ml",
+        date: "2005",
+        id: "BBJ003250",
+        strikedOffPrice: "₹489",
+        price: 489,
+        discount: "12 % OFF",
+        rate: 3,
+        brand: "Vaadi Herbals",
+
+       
+    },
+    {
+        image: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/b/e/be97.jpg",
+        title: "O3+ Volcano Scrub",
+        date: "2006",
+        id: "BBJ003250",
+        strikedOffPrice: "₹489",
+        price: 489,
+        discount: "12 % OFF",
+        rate: 4,
+        brand: "Vaadi Herbals",
+
+      
+    },
+    {
+        image: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/b/e/be106.jpg",
+        title: "Beeone Papaya Scrub",
+        date: "2007",
+        id: "BBJ003250",
+        strikedOffPrice: "₹119",
+        price: 119,
+        discount: "12 % OFF",
+        rate: 5,
+        brand: "Vaadi Herbals",
+
+    },
+    {
+        image: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/j/6/j6.jpg",
+        title: "Joy Hydra Refresh Ultra Light Gel Moisturiser",
+        date: "2008",
+        id: "BBJ003250",
+        strikedOffPrice: "₹189",
+        price: 189,
+        discount: "12 % OFF",
+        rate: 2,
+        brand: "Vaadi Herbals",
+
+      
+    },
+    {
+        image: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/b/e/be85.jpg",
+        title: "Beeone Lemon Facial Gel",
+        date: "2009",
+        id: "BBJ003250",
+        strikedOffPrice: "₹389",
+        price: 389,
+        discount: "12 % OFF",
+        rate: 3,
+        brand: "Vaadi Herbals",
+
+       
+    },
+    {
+        image: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/l/a/la9.jpg",
+        title: "L'Oreal Paris White Perfect Scrub",
+        date: "2011",
+        id: "BBJ003250",
+        strikedOffPrice: "₹489",
+        price: 489,
+        discount: "12 % OFF",
+        rate: 4,
+        brand: "Vaadi Herbals",
+
+      
+    },
+    {
+        image: "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/s/1/s1_1.jpg",
+        title: "Shahnaz Husain Gold 2-In-1 Anti Ageing Gel",
+        date: "2002",
+        id: "BBJ003250",
+        strikedOffPrice: "₹1489",
+        price: 1489,
+        discount: "12 % OFF",
+        rate: 3,
+        brand: "Vaadi Herbals",
+
+      
+    },
+
+]
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,158 @@
-import Image from 'next/image'
+'use client'
 import styles from './page.module.css'
+import {data} from "./db"
+import React from 'react'
+
+
 
 export default function Home() {
+const [sort,setsort]=React.useState(data)
+
+function sorting(sortingtype){
+  let sorted=[]
+  if(sortingtype==="asc"){
+    sorted=[...sort].sort((a,b)=>{return a["price"]-b["price"]})
+  }else if(sortingtype==="dsc"){
+    sorted=[...sort].sort((a,b)=>{return b["price"]-a["price"]})
+  }else if(sortingtype==="date"){
+    sorted=[...sort].sort((a,b)=>{return b["date"]-a["date"]})
+  }else{
+    sorted=[...sort].sort((a,b)=>{return b["rate"]-a["rate"]})
+  }
+  
+ 
+  setsort(sorted)
+}
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+    <main >
+      <nav className={styles.navbar}>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <img src="https://cdn-icons-png.flaticon.com/128/9129/9129418.png"  alt="logo"/>
         </div>
-      </div>
+        <div className={styles.options}>
+          <li>Home</li>
+          <li>My Campaign</li>
+        </div>
+        <div>
+        <img src="https://cdn-icons-png.flaticon.com/128/3602/3602123.png"  alt="bell_logo"/>
+        <img src="https://cdn-icons-png.flaticon.com/128/3293/3293334.png"  alt="auth_logo"/>
+        </div>
+      </nav>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+<section className={styles.dashboard}>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+<div className={styles.filtering}>
+<div>
+  <h4>POPULARITY</h4>
+  <li> <input type='checkbox' value={"Nano"}/>Nano(1k-9k)</li>
+  <li> <input type='checkbox' value={"micro"}/>Micro(100k-999k)</li>
+  <li> <input type='checkbox' value={"macro"}/>Macro(1M-10M)</li>
+  <li> <input type='checkbox' value={"custom"}/>Custom</li>
+  <input type="range" min="0" max="100"  className={styles.slider}id="myRange"></input>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+ 
+</div>
+<div>
+  <h4>CAMPAIGN PREFERANCE</h4>
+  <li> <input type='checkbox' value={"barter"}/>Barter</li>
+  <li> <input type='checkbox' value={"paid"}/>Paid</li>
+</div>
+<div>
+  <h4>PLATFORM</h4>
+  <li> <input type='checkbox' value={"youtube"}/>YouTube</li>
+  <li> <input type='checkbox' value={"instagram"}/>Instagram</li>
+</div>
+<div>
+  <h4>ENGAGEMENT RATE</h4>
+  <input type="range" min="0" max="100"  className={styles.slider}id="myRange"></input>
+</div>
+<div>
+  <h4>AVERAGE</h4>
+  <input type="range" min="0" max="100"  className={styles.slider}id="myRange"></input>
+</div>
+<div>
+  <h4>CATEGORIES</h4>
+  <li> <input type='checkbox' value={"auto"}/>Auto & Vehicles</li>
+  <li> <input type='checkbox' value={"Animation"}/>Animation</li>
+  <li> <input type='checkbox' value={"agriculture"}/>Aglriculture & Allied sector</li>
+  <li> <input type='checkbox' value={"Arts"}/>Arts & Crafts</li>
+  <li> <input type='checkbox' value={"Beauty"}/>Beauty</li>
+  <li> <input type='checkbox' value={"blogs"}/>Blogs & Travel</li>
+</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+
+</div>
+
+
+
+<div className={styles.rightsection}>
+  {/* right side for upper buttons */}
+<div className={styles.uppersection}>
+<div>
+  <label>Sort by <br/>
+    <button onClick={()=>sorting("asc")} > Low to High</button>
+    <button onClick={()=>sorting("dsc")}>High to Low</button>
+    <button onClick={()=>sorting("date")} >Newest</button>
+    <button onClick={()=>sorting("rate")}>Popular</button>
+
+  </label>
+</div>
+<div>
+
+<button>SELECT ALL</button>
+<button>INVITE</button>
+<button>ADD CAMPAIGN</button>
+</div>
+
+</div>
+{/* for product compaigns */}
+<div>
+    {sort.map((el,i)=>{
+  return <div className={styles.cards}>
+    <div> <img src={el.image}/></div>
+    
+    <div>
+    <div>
+      <h5>{el.title}</h5>
+     <p> This uniquely made Facial Scrub gently cleanses facial skin without removing moisture and makes the skin appear baby soft and pampered.
+     </p>
+     <p>Market price :Rs{el.price}</p>
+     <p>Launch At :{el.date}</p>
+      <div  className={styles.acceptbtn}> <button >Accepted</button>
+  <button>Barter</button></div>
+    </div>
+    <div className={styles.cardbtnright}>
+    <button>DETAIL</button>
+  <button>INVITE</button>
+  <button>ANALYSE</button>
+    </div>
+    </div>
+    </div>
+    })}
+    
+  </div>
+
+
+
+</div>
+
+
+</section>
+
+   
     </main>
   )
+}
+
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await axios(`/product/getall`);
+  const d = await res.data;
+  const data = d.products;
+
+  // Pass data to the page via props
+  return { props: { data } };
 }
